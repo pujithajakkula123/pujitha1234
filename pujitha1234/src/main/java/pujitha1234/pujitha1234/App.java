0b1da4cb289d506e0f944b43f1371f96b2e5a0b1
ADMIN <ADMIN@DESKTOP-LOB2K5S>
2021-03-11 14:05:03 +0530

    project123

---
+++ pujitha1234/src/main/java/pujitha1234/pujitha1234/App.java
@@ -0,0 +1,13 @@
+package pujitha1234.pujitha1234;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
